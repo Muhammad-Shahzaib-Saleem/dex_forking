26094d42e752d2c14b99ecd6ace27ffa92410849
mAc BoOk pRo <dotenterprises@mAcs-MacBook-Pro.local>
2023-11-28 10:10:04 +0500

    Adding Deploment Script for Factory Contract Swap

---
+++ scripts/2_deploy_contracts.ts
@@ -0,0 +1,30 @@
+import { ethers,hre } from "hardhat";
+import { SwapERC20__factory } from "../typechain-types";
+
+async function main() {
+  const adminAddress = "0xF48C993dea34AFbe3e0Bb28b36A23675e28ec52a";
+  const feeCollectingAddress = "0xF48C993dea34AFbe3e0Bb28b36A23675e28ec52a";
+
+  const swap_factory = await hre.ethers.getContractFactory("SwapFactory");
+  const swapFactory = await swap_factory.deploy(adminAddress);
+
+  await swapFactory.deployed();
+
+
+  
+
+  await swapFactory.setFeeTo(feeCollectingAddress);
+
+
+  console.log(
+    `admin address ${adminAddress
+    } deployed to ${swapFactory.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
